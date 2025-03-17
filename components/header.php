2840d022a84f141bfdb6a6c42e1315419660845e
user.walid-ka <user.walid.kouider-ayad@heptacom.de>
2025-03-17 17:12:33 +0100

    fixed

--- components/header.php
+++ components/header.php
@@ -16,7 +16,7 @@ error_reporting(E_ALL);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Pagee</title>
+    <title>Home Page</title>
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 
